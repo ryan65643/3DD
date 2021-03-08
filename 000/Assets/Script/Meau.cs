48703a0ef74b2ec709f5a704295733f98f1c628e
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-03-08 22:44:17 +0800

    0000

--- 000/Assets/Script/Meau.cs
+++ 000/Assets/Script/Meau.cs
@@ -6,21 +6,73 @@ public class Meau : MonoBehaviour
     public GameObject Backpage;
     public GameObject Skill;
     public GameObject Mea;
+    public CanvasGroup CanSkill;
+    public CanvasGroup CanBackpage;
+    public CanvasGroup CanMeau;
 
     private void Awake()
     {
         Skill = GameObject.Find("技能層");
         Backpage = GameObject.Find("背包層");
         Mea = GameObject.Find("介面層");
+        CanSkill = GameObject.Find("技能層").GetComponent<CanvasGroup>();
+        CanBackpage = GameObject.Find("背包層").GetComponent<CanvasGroup>();
+        CanMeau = GameObject.Find("介面層").GetComponent<CanvasGroup>();
+    }
+
+    public void openmeau()
+    {
+        CanMeau.alpha = 1;
+        CanMeau.interactable = false;
+        CanMeau.blocksRaycasts = false;
+    }
+
+    public void closemeau()
+    {
+        CanMeau.alpha = 0;
+        CanMeau.interactable = true;
+        CanMeau.blocksRaycasts = true;
     }
 
     public void openskill()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            print("123");
-            Mea.SetActive(false);
-            Skill.SetActive(true);
-        }
+        CanSkill.alpha = 1;
+        CanSkill.interactable = false;
+        CanSkill.blocksRaycasts = false;
+    }
+
+    public void closeskill()
+    {
+        //CanSkill.alpha = 0;
+        //CanSkill.interactable = true;
+        //CanSkill.blocksRaycasts = true;
+        //openskill();
+        print("123");
+    }
+
+    public void quitskill()
+    {
+        closeskill();
+        openmeau();
+    }
+
+    public void openBackpage()
+    {
+        CanBackpage.alpha = 1;
+        CanBackpage.interactable = false;
+        CanBackpage.blocksRaycasts = false;
+    }
+
+    public void closeBackpage()
+    {
+        CanBackpage.alpha = 0;
+        CanBackpage.interactable = true;
+        CanBackpage.blocksRaycasts = true;
+    }
+
+    public void quitBackpage()
+    {
+        closeBackpage();
+        openmeau();
     }
 }
