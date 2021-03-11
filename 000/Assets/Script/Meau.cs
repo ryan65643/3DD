016db4510a1ea7627232f51366ed481e6419b39c
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-03-11 11:17:09 +0800

    0000

--- 000/Assets/Script/Meau.cs
+++ 000/Assets/Script/Meau.cs
@@ -19,35 +19,34 @@ public class Meau : MonoBehaviour
         CanBackpage = GameObject.Find("背包層").GetComponent<CanvasGroup>();
         CanMeau = GameObject.Find("介面層").GetComponent<CanvasGroup>();
     }
-
+    #region 介面按鈕切換
     public void openmeau()
     {
         CanMeau.alpha = 1;
-        CanMeau.interactable = false;
-        CanMeau.blocksRaycasts = false;
+        CanMeau.interactable = true;
+        CanMeau.blocksRaycasts = true;
     }
 
     public void closemeau()
     {
         CanMeau.alpha = 0;
-        CanMeau.interactable = true;
-        CanMeau.blocksRaycasts = true;
+        CanMeau.interactable = false;
+        CanMeau.blocksRaycasts = false;
     }
 
     public void openskill()
     {
         CanSkill.alpha = 1;
-        CanSkill.interactable = false;
-        CanSkill.blocksRaycasts = false;
+        CanSkill.interactable = true;
+        CanSkill.blocksRaycasts = true;
     }
 
     public void closeskill()
     {
-        //CanSkill.alpha = 0;
-        //CanSkill.interactable = true;
-        //CanSkill.blocksRaycasts = true;
-        //openskill();
-        print("123");
+        CanSkill.alpha = 0;
+        CanSkill.interactable = false;
+        CanSkill.blocksRaycasts = false;
+
     }
 
     public void quitskill()
@@ -59,15 +58,15 @@ public class Meau : MonoBehaviour
     public void openBackpage()
     {
         CanBackpage.alpha = 1;
-        CanBackpage.interactable = false;
-        CanBackpage.blocksRaycasts = false;
+        CanBackpage.interactable = true;
+        CanBackpage.blocksRaycasts = true;
     }
 
     public void closeBackpage()
     {
         CanBackpage.alpha = 0;
-        CanBackpage.interactable = true;
-        CanBackpage.blocksRaycasts = true;
+        CanBackpage.interactable = false;
+        CanBackpage.blocksRaycasts = false;
     }
 
     public void quitBackpage()
@@ -75,4 +74,5 @@ public class Meau : MonoBehaviour
         closeBackpage();
         openmeau();
     }
+    #endregion
 }
--- 000/Assets/Script/Prop.cs
+++ 000/Assets/Script/Prop.cs
@@ -2,17 +2,20 @@
 
 public class Prop : MonoBehaviour
 {
-    [Header("左線條的長度")]
-    public float Length0 = 0.376f;
-    [Header("上線條的長度")]
-    public float Length90 = 0.376f;
-
+    [Header("一般條的長度")]
+    public float Length0 = 0.443f;
+    [Header("完美條的長度")]
+    public float Length90 = 0.02f;
+    [Header("糟糕條的長度")]
+    public float Length180 = -0.443f;
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, 0.3f);
         Gizmos.DrawRay(transform.position,Vector3.left*Length0);
         Gizmos.color = new Color(0, 1, 0, 0.3f);
-        Gizmos.DrawRay(transform.position, Vector3.up * Length90);
+        Gizmos.DrawRay(transform.position, Vector3.left * Length90);
+        Gizmos.color = new Color(0, 0, 1, 0.3f);
+        Gizmos.DrawRay(transform.position, Vector3.left * Length180);
     }
 
 }
