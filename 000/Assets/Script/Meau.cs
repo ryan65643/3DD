6e23b721ada4e01a1437b652a4e7b4e140f84378
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2021-03-11 21:29:54 +0800

    12313

--- 000/Assets/Script/Meau.cs
+++ 000/Assets/Script/Meau.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Meau : MonoBehaviour
 {
     public GameObject Backpage;
     public GameObject Skill;
     public GameObject Mea;
+    public GameObject Judgmentarea;
+    public GameObject[] Gam;
+    [Header("生成點")]
+    public GameObject BronProp;
+    [Header("間隔時間"), Range(0f, 10f)]
+    public float bye = 10f;
     public CanvasGroup CanSkill;
     public CanvasGroup CanBackpage;
     public CanvasGroup CanMeau;
+    public Transform Prop;
+    private float Speed = 10;
+    private bool a;
 
+    private void Start()
+    {
+        a = true;
+        Judgmentarea.SetActive(false);
+        InvokeRepeating("born", 0, bye);
+    }
+    private void Update()
+    {
+        OpenCkeck();
+    }
     private void Awake()
     {
+        Judgmentarea = GameObject.Find("判斷區域");
         Skill = GameObject.Find("技能層");
         Backpage = GameObject.Find("背包層");
         Mea = GameObject.Find("介面層");
         CanSkill = GameObject.Find("技能層").GetComponent<CanvasGroup>();
         CanBackpage = GameObject.Find("背包層").GetComponent<CanvasGroup>();
         CanMeau = GameObject.Find("介面層").GetComponent<CanvasGroup>();
+        BronProp = GameObject.Find("生成區域");
+        Gam = Resources.LoadAll<GameObject>("");
+
+
     }
     #region 介面按鈕切換
     public void openmeau()
@@ -75,4 +100,38 @@ public class Meau : MonoBehaviour
         openmeau();
     }
     #endregion
+    /// <summary>
+    /// 生成方塊
+    /// </summary>
+    private void born()
+    {
+        int r = Random.Range(0, Gam.Length);
+        Transform point = BronProp.transform;
+        if (a)
+        {
+            GameObject temp = Instantiate(Gam[r], point.position, point.rotation).gameObject;
+        }
+
+    }
+    private void OpenCkeck()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Judgmentarea.SetActive(true);
+            che();
+        }
+        else Judgmentarea.SetActive(false);
+
+    }
+
+    private void che()
+    {
+        RaycastHit2D Hit = Physics2D.Raycast(transform.position, Vector2.left, 1 << 8);
+        if (Hit && Hit.transform.name == "判斷區域")
+        {
+            print("123");
+        }
+    }
+
+
 }
--- 000/Assets/Script/Prop.cs
+++ 000/Assets/Script/Prop.cs
@@ -3,19 +3,42 @@
 public class Prop : MonoBehaviour
 {
     [Header("一般條的長度")]
-    public float Length0 = 0.443f;
+    public float Length0 = 0.378f;
     [Header("完美條的長度")]
     public float Length90 = 0.02f;
     [Header("糟糕條的長度")]
-    public float Length180 = -0.443f;
+    public float Length180 = -0.378f;
+    private float Speed = 10;
+    private bool Born;
+    private void Start()
+    {
+        Born = true;
+    }
+    private void Update()
+    {
+        move();
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, 0.3f);
-        Gizmos.DrawRay(transform.position,Vector3.left*Length0);
+        Gizmos.DrawRay(transform.position, Vector3.left * Length0);
         Gizmos.color = new Color(0, 1, 0, 0.3f);
         Gizmos.DrawRay(transform.position, Vector3.left * Length90);
         Gizmos.color = new Color(0, 0, 1, 0.3f);
         Gizmos.DrawRay(transform.position, Vector3.left * Length180);
     }
+    /// <summary>
+    /// 方塊移動
+    /// </summary>
+    private void move()
+    {
+        if (Born)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * Speed);
+        }
+
+    }
 
 }
+
